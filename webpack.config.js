4b56b99e7642330ec6170145419da806ac70be68
Will Quanstrom <wquanstr215@gmail.com>
2020-02-23 21:50:01 -0800

    initial workflow setup

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'CHANGENAME',
+      title: 'modern-porfolio',
       template: './src/index.html',
       inject: 'body'
     })
